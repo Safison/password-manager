73fa8fbb2b99b1bdd29b1acc91793b9c0712c1fe
Angelo Rohanathan <angelorohanathan3@gmail.com>
2025-02-20 12:49:40 +0000

    Implemented entry util functions

--- test/test_password_entry.py
+++ test/test_password_entry.py
@@ -1,4 +1,24 @@
-from utility.password_entry import get_identifier, get_credentials, save_secret
+from utility.password_entry import get_identifier, get_and_save_credentials
 
-def test_get_identifier():
+def test_get_identifier_if_identifier_does_not_exist_ask_for_username_and_password_works_returns_id():
+    input = 'Angelo-Anas-test-ident123'
+    expected = input
+    result = get_identifier()
+    assert result == expected
+    
+def test_get_identifier_if_identifier_exists_output_valid_message():
+    input = 'Angelo-Anas-test-ident'
+    expected = "This identifier already exists"
+    result = get_identifier()
+    assert result == expected
+
+
+def test_get_credentials_asks_for_username_and_password():
+    input_user = 'angelo'
+    input_password = 123
+    expected = 'Angelo-Anas-test-ident5'
+    result = get_and_save_credentials(expected)
+    assert result == expected
+    
+    
     
--- utility/password_entry.py
+++ utility/password_entry.py
@@ -4,11 +4,37 @@
 # 2 - creadetials function
 # 3 - save entries function
 
+import boto3
+import maskpass
+
 def get_identifier():
-    pass
+    identifier = input('Please enter your identifier name: ')
+    client = boto3.client('secretsmanager')
+    try:
+        response = client.describe_secret(SecretId=identifier)
+        return 'This identifier already exists'
+    except client.exceptions.ResourceNotFoundException as e:
+        return identifier
 
-def get_credentials():
-    pass
+    
 
-def save_secret():
-    pass
+def get_and_save_credentials(identifier):
+    client = boto3.client('secretsmanager')
+    get_username = input('Please enter your username: ')
+    get_pwd = maskpass.askpass(prompt="Please enter your password: ", mask="#")
+    username_str = '{"username":'
+    password_str = ',"password":'
+    end_str = '}'
+    secretstring = username_str + get_username + password_str + get_pwd + end_str
+    response = client.create_secret(
+    Name=identifier,
+    SecretString=secretstring
+    )
+    print(response)
+    
+    return response['Name']
+
+# get_and_save_credentials('Angelo-Anas-test-ident1')
+
+
+
