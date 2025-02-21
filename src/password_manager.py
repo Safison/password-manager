8dccca26460ae6c8ea95f1c030a57b0b138d7420
Angelo Rohanathan <angelorohanathan3@gmail.com>
2025-02-21 12:31:21 +0000

    Added if statements to main code

--- src/password_manager.py
+++ src/password_manager.py
@@ -2,15 +2,29 @@ from utility.password_entry import get_identifier, get_and_save_credentials
 
 
 def main():
-    identifier = input('Please enter your identifier name: ')
-    get_id = get_identifier(identifier)
-    if get_id == identifier:
-        get_credentials = get_and_save_credentials(identifier)
-        if get_credentials == identifier:
-            return 'Secret created successfully'
-            
+    flag = True
+    while flag:
+        initial_input = input('Please specify [e]ntry, [r]etrieval, [d]eletion, [l]isting or e[x]it:')
+        if initial_input == 'e':
+            identifier = input('Please enter your identifier name: ')
+            get_id = get_identifier(identifier)
+            if get_id == identifier:
+                get_credentials = get_and_save_credentials(identifier)
+                if get_credentials == identifier:
+                    return 'Secret created successfully'
+                    
+                else:
+                    print('Unsuccessful creation, try again')
+            else:
+                return "Identifier already exist"
+        elif initial_input == 'r':
+        elif initial_input == 'd':
+        elif initial_input == 'l':
+        elif initial_input == 'x':
+            print('Thank you. Goodbye.')
+            flag = False
         else:
-            print('Unsuccessful creation, try again')
-    else:
-        return "Identifier already exist"
+            return 'Invalid input. Please specify [e]ntry, [r]etrieval, [d]eletion, [l]isting or e[x]it:'
+            
+        
 
