567100d4077b53bacad5234aa765732f28161143
Angelo Rohanathan <angelorohanathan3@gmail.com>
2025-02-21 11:43:54 +0000

    Fixed retrieval util function

--- utility/retrieve_password.py
+++ utility/retrieve_password.py
@@ -1,27 +1,27 @@
 import boto3
 import ast
+import json
 
-def password_retrieval():
-    client = boto3.client('secretsmanager')
+client = boto3.client('secretsmanager')
+
+def password_retrieval(client):
     id_input = input('Specify secret to retrieve:')
     try:
         response = client.get_secret_value(
         SecretId=id_input)
-
-        #response['SecretString']['Name'] == id_input
-        sec_string_dict = ast.literal_eval(response['SecretString'])
-        user_id = sec_string_dict['username']
-        password = sec_string_dict['password']
+        sec_str = response['SecretString']
+        comma_index = sec_str.rfind(',')
+        user_str = sec_str[1:comma_index] + '\n'
+        pass_str = sec_str[comma_index+1:len(sec_str)-1]
         file = open("secrets.txt", "w")
-        
-        file.writelines([f'user_id: {user_id}',f'password: {password}'])
+        file.writelines([user_str, pass_str])
         file.close()
         return (f'secrets stored in {file.name}')
     
     except client.exceptions.ResourceNotFoundException as e:
         return 'Secret not found'
 
-password_retrieval()
+# password_retrieval()
 
 
     
